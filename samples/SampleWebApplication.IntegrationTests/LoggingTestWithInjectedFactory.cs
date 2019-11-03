46668ca9d00533af39830bcf32bfbad71ae1357b
Alessio Franceschelli <alessio@franceschelli.me>
2019-11-03 22:15:21 +0000

    Helpers to simplify AspNetCore integration tests

--- samples/SampleWebApplication.IntegrationTests/LoggingTestWithInjectedFactory.cs
+++ samples/SampleWebApplication.IntegrationTests/LoggingTestWithInjectedFactory.cs
@@ -1,7 +1,7 @@
 using MELT;
-using MELT.Xunit;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 using Xunit;
@@ -13,6 +13,47 @@ namespace SampleWebApplication.Tests
         private readonly CustomWebApplicationFactory<Startup> _factory;
 
         public LoggingTestWithInjectedFactory(CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+            // In this case the factory will be resused for all tests, so the sink will be shared as well.
+            // We can clear the sink before each test execution, as xUnit will not run this tests in parallel.
+            _factory.GetTestSink().Clear();
+        }
+
+        [Fact]
+        public async Task ShouldLogHelloWorld()
+        {
+            // Arrange  
+
+            // Act
+            await _factory.CreateDefaultClient().GetAsync("/");
+
+            // Assert
+            var log = Assert.Single(_factory.GetTestSink().LogEntries);
+            // Assert the message rendered by a default formatter
+            Assert.Equal("Hello World!", log.Message);
+        }
+
+        [Fact]
+        public async Task ShouldUseScope()
+        {
+            // Arrange  
+
+            // Act
+            await _factory.CreateDefaultClient().GetAsync("/");
+
+            // Assert
+            var log = Assert.Single(_factory.GetTestSink().LogEntries);
+            // Assert the scope rendered by a default formatter
+            Assert.Equal("I'm in the GET scope", log.Scope.Message);
+        }
+    }
+
+    public class LoggingTestWithInjectedFactoryWithoutHelpers : IClassFixture<CustomWebApplicationFactoryWithoutHelpers<Startup>>
+    {
+        private readonly CustomWebApplicationFactoryWithoutHelpers<Startup> _factory;
+
+        public LoggingTestWithInjectedFactoryWithoutHelpers(CustomWebApplicationFactoryWithoutHelpers<Startup> factory)
         {
             _factory = factory;
             // In this case the factory will be resused for all tests, so the sink will be shared as well.
@@ -49,7 +90,7 @@ namespace SampleWebApplication.Tests
         }
     }
 
-    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup>
+    public class CustomWebApplicationFactoryWithoutHelpers<TStartup> : WebApplicationFactory<TStartup>
          where TStartup : class
     {
         public ITestSink Sink { get; } = MELTBuilder.CreateTestSink(options => options.FilterByNamespace(nameof(SampleWebApplication)));
@@ -58,5 +99,22 @@ namespace SampleWebApplication.Tests
         {
             builder.ConfigureLogging(logging => logging.AddTestLogger(Sink));
         }
+    }
+
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup>
+         where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            //builder.UseTestLogging();
+            builder.ConfigureLogging(logging => logging.AddTestLogger());
+        }
+    }
+
+    public static class WebApplicationFactoryExtensions
+    {
+        public static ITestSink GetTestSink<TStartup>(this WebApplicationFactory<TStartup> factory)
+            where TStartup : class
+            => factory.Services.GetRequiredService<ITestSink>();
     }
 }
