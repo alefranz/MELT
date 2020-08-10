8f4afba491d4bbcffbce4943cef9aa20af1704a2
Alessio Franceschelli <alessio@franceschelli.me>
2020-08-10 09:16:31 +0100

    More XML docs

--- src/MELT/TestLoggerFactory.cs
+++ src/MELT/TestLoggerFactory.cs
@@ -5,13 +5,18 @@ using System.Collections.Generic;
 
 namespace MELT
 {
+    /// <summary>
+    /// A logger factory to be used to capture log messages and scopes in a test sink.
+    /// </summary>
     public class TestLoggerFactory : ITestLoggerFactory
     {
         private readonly ILoggerFactory _loggerFactory;
         private readonly ServiceProvider _serviceProvider;
 
         [Obsolete]
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
         public TestLoggerFactory(ITestSink sink)
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
         {
             var serviceCollection = new ServiceCollection();
             serviceCollection.AddLogging(builder => builder.AddTestLogger(sink));
@@ -20,15 +25,22 @@ namespace MELT
         }
 
         [Obsolete]
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
         public IEnumerable<LogEntry> LogEntries => Sink.LogEntries;
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 
         [Obsolete]
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
         public IEnumerable<BeginScope> Scopes => Sink.Scopes;
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 
         private TestLoggerFactory(ILoggerFactory loggerFactory, ServiceProvider serviceProvider)
         {
             _loggerFactory = loggerFactory;
             _serviceProvider = serviceProvider;
+
+            // Unfortunately there is a breaking change in 3.1 which causes an exception on BeginScope:
+            // System.TypeLoadException : Could not load type 'Microsoft.Extensions.Logging.Abstractions.Internal.NullScope' from assembly 'Microsoft.Extensions.Logging.Abstractions, Version=3.1.0.0, Culture=neutral, PublicKeyToken=adb9793829ddae60'.
 
             var dependencyVersion = loggerFactory.GetType().Assembly.GetName().Version;
             var abstractionsVersion = typeof(ILoggerFactory).Assembly.GetName().Version;
@@ -40,23 +52,33 @@ namespace MELT
             }
         }
 
+        /// <summary>
+        /// The <see cref="ITestLoggerSink"/> which gives access to the captured logs.
+        /// </summary>
         public ITestLoggerSink Sink => _serviceProvider.GetRequiredService<ITestLoggerSink>();
 
+        /// <inheritdoc/>
         public void Dispose()
         {
             _serviceProvider.Dispose();
         }
 
+        /// <inheritdoc/>
         public ILogger CreateLogger(string categoryName)
         {
             return _loggerFactory.CreateLogger(categoryName);
         }
 
+        /// <inheritdoc/>
         public void AddProvider(ILoggerProvider provider)
         {
             _loggerFactory.AddProvider(provider);
         }
 
+        /// <summary>
+        /// Create a default logger factory to be used to capture log messages and scopes in a test sink.
+        /// </summary>
+        /// <returns>The test logger factory.</returns>
         public static ITestLoggerFactory Create()
         {
             var serviceCollection = new ServiceCollection();
@@ -66,6 +88,11 @@ namespace MELT
             return new TestLoggerFactory(loggerFactory, serviceProvider);
         }
 
+        /// <summary>
+        /// Create a configured logger factory to be used to capture log messages and scopes in a test sink.
+        /// </summary>
+        /// <param name="configure">A delegate used to configure the <see cref="ILoggingBuilder"/>.</param>
+        /// <returns>The test logger factory.</returns>
         public static ITestLoggerFactory Create(Action<ILoggingBuilder> configure)
         {
             var serviceCollection = new ServiceCollection();
@@ -76,6 +103,11 @@ namespace MELT
             return new TestLoggerFactory(loggerFactory, serviceProvider);
         }
 
+        /// <summary>
+        /// Create a configured logger factory to be used to capture log messages and scopes in a test sink.
+        /// </summary>
+        /// <param name="configure">A delegate used to configure the <see cref="TestLoggerOptions"/>.</param>
+        /// <returns>The test logger factory.</returns>
         public static ITestLoggerFactory Create(Action<TestLoggerOptions> configure)
         {
             var serviceCollection = new ServiceCollection();
