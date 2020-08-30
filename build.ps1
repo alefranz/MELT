402420cf74b0f12df9ef01502ee2095ae5e9f54a
Alessio Franceschelli <alessio@franceschelli.me>
2020-08-30 22:25:08 +0100

    Build script

---
+++ build.ps1
@@ -0,0 +1,1 @@
+dotnet format && dotnet build && dotnet test
--- samples/current/NLog/SampleWebApplicationNLog/Program.cs
+++ samples/current/NLog/SampleWebApplicationNLog/Program.cs
@@ -1,8 +1,8 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NLog.Web;
-using System;
 
 namespace SampleWebApplicationNLog
 {
