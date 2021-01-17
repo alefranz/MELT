c1311e0c53e0e1daaf0cb9a09a89caac8a738759
Alessio Franceschelli <alessio@franceschelli.me>
2021-01-17 20:52:38 +0000

    Suggest how to automatically fix formatting errors

--- samples/3.1/SampleWebApplication3_1/Program.cs
+++ samples/3.1/SampleWebApplication3_1/Program.cs
@@ -6,7 +6,7 @@ namespace SampleWebApplication3_1
     public class Program
     {
         public static void Main(string[] args)
-        {
+            {
             CreateHostBuilder(args).Build().Run();
         }
 
