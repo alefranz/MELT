dda76a46258135d77c32a404eb2c91b5767382c7
Alessio Franceschelli <alessio@franceschelli.me>
2023-08-06 22:22:08 +0100

    Add support for Xunit 2.5.0 (#38)

---
+++ samples/xunit-2.4.2/SampleLibraryX/Sample.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace SampleLibraryX
+{
+    public class Sample
+    {
+        private readonly ILogger<Sample> _logger;
+
+        public Sample(ILogger<Sample> logger)
+        {
+            _logger = logger;
+        }
+
+        public void DoSomething()
+        {
+            _logger.LogInformation("The answer is {number}", 42);
+        }
+
+        public void DoExceptional()
+        {
+            var exception = new ArgumentNullException("foo");
+            _logger.LogError(exception, "There was a {error}", "problem");
+        }
+    }
+}
